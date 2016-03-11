8719e2f6c9d495e4171b1d58e4f79f767825348f
zhupeng <178280101@qq.com>
2016-03-11 19:58:52 +0800

    first commit

---
+++ src/com/zp/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.zp;
+
+/**
+ * Created by zhupeng on 2016/3/11.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+
+    }
+}
